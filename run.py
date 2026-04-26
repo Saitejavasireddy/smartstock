2559c51d0a2ef35af73e259a0d4a0cfe6d5b5cba
Sai Teja Vasireddy <teja@Sais-MacBook-Air-3.local>
2026-04-26 13:39:45 -0500

    Auto create tables on startup

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from app.models import User, Product, Transaction, Alert
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     with app.app_context():
         print("Connecting to:", app.config['SQLALCHEMY_DATABASE_URI'])
